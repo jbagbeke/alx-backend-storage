c77c129e4bc1eee19f303f40898477e2661e2d13
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-23 18:47:07 +0000

    0x00. MySQL advanced Mandoatory task 10

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,21 @@
+-- Creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0 if the second
+-- number is equal to 0.
+
+DELIMITER $$
+
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+    DECLARE div_result FLOAT;
+
+    IF b = 0 THEN
+        SET div_result = 0;
+    ELSE
+        SET div_result = a / b;
+    END IF;
+
+    RETURN div_result;
+END;
+$$
