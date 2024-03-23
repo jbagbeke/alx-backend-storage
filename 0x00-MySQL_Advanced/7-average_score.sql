c284e50297c225cf8c318eb0bffcdc9d92cca729
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-23 17:50:04 +0000

    0x00. MySQL advanced Mandoatory task 7

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,18 @@
+-- Creates a stored procedure ComputeAverageScoreForUser
+-- that computes and store the average score for a student.
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE user_average FLOAT;
+
+    SELECT AVG(score) INTO user_average
+    FROM corrections
+    WHERE user_id = user_id;
+
+    UPDATE users SET average_score = user_average WHERE id = user_id; 
+END;
+$$
