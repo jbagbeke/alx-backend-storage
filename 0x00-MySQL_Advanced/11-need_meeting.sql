ccb94c50d297378eba77d1537ef1d8fb33a996a8
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-23 19:01:51 +0000

    0x00. MySQL advanced Mandoatory task 11

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,9 @@
+-- Creates a view need_meeting that lists all students
+-- that have a score under 80 (strict) and no last_meeting
+-- or more than 1 month.
+
+DELIMITER $$
+
+CREATE VIEW need_meeting AS SELECT name FROM
+students WHERE score < 80
+AND (last_meeting IS NULL OR DATEDIFF(last_meeting, CURDATE()) > 31); 
