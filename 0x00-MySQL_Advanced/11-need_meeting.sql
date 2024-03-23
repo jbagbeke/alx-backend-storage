12a64c5eeded8e9f11be3cebfcce910f2b029ba3
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-23 19:04:26 +0000

    0x00. MySQL advanced Mandoatory task 11

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -6,4 +6,4 @@ DELIMITER $$
 
 CREATE VIEW need_meeting AS SELECT name FROM
 students WHERE score < 80
-AND (last_meeting IS NULL OR DATEDIFF(last_meeting, CURDATE()) > 31); 
+AND (last_meeting IS NULL OR DATEDIFF(CURDATE() ,last_meeting) > 30);
