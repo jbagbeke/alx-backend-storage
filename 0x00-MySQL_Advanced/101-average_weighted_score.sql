02da8051b4845d7f7853532bba4b1b84c441c9c7
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-24 15:38:17 +0000

    0x00. MySQL advanced ADVANCED task 12

---
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,33 @@
+-- Creates a stored procedure ComputeAverageWeightedScoreForUser
+-- that computes and store the average weighted score for every student
+
+DELIMITER $$
+
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+BEGIN
+    DECLARE project_weight_sum INT;
+    DECLARE score_project_weight FLOAT;
+    DECLARE user_count INT;
+
+    SELECT COUNT(*) INTO user_count
+    FROM users;
+
+    SELECT SUM(projects.weight * corrections.score)
+    INTO score_project_weight
+    FROM projects
+    JOIN corrections
+    ON corrections.user_id = user_id AND projects.id = corrections.project_id;
+
+    SELECT SUM(projects.weight)
+    INTO project_weight_sum
+    FROM corrections
+    JOIN projects
+    ON corrections.user_id = user_id AND corrections.project_id = projects.id;
+
+    UPDATE users
+    SET average_score = (score_project_weight / project_weight_sum)
+    WHERE id = user_id;
+END;
+$$
