1669d70ecf2378cba5e0b20d683ebf6e4c1d12ea
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-24 22:21:59 +0000

    0x00. MySQL advanced ADVANCED task 13

--- 0x00-MySQL_Advanced/101-average_weighted_score.sql
+++ 0x00-MySQL_Advanced/101-average_weighted_score.sql
@@ -3,31 +3,39 @@
 
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUsers;
 
-CREATE PROCEDURE ComputeAverageWeightedScoreForUser
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers()
 BEGIN
     DECLARE project_weight_sum INT;
     DECLARE score_project_weight FLOAT;
     DECLARE user_count INT;
+    DECLARE loop_count INT;
 
     SELECT COUNT(*) INTO user_count
     FROM users;
 
-    SELECT SUM(projects.weight * corrections.score)
-    INTO score_project_weight
-    FROM projects
-    JOIN corrections
-    ON corrections.user_id = user_id AND projects.id = corrections.project_id;
+    SET loop_count = (user_count - 1);
+    
+    WHILE loop_count <= user_count DO
+        SELECT SUM(projects.weight * corrections.score)
+        INTO score_project_weight
+        FROM projects
+        JOIN corrections
+        ON corrections.user_id = loop_count AND projects.id = corrections.project_id;
 
-    SELECT SUM(projects.weight)
-    INTO project_weight_sum
-    FROM corrections
-    JOIN projects
-    ON corrections.user_id = user_id AND corrections.project_id = projects.id;
+        SELECT SUM(projects.weight)
+        INTO project_weight_sum
+        FROM corrections
+        JOIN projects
+        ON corrections.user_id = loop_count AND corrections.project_id = projects.id;
 
-    UPDATE users
-    SET average_score = (score_project_weight / project_weight_sum)
-    WHERE id = user_id;
+        UPDATE users
+        SET average_score = (score_project_weight / project_weight_sum)
+        WHERE id = loop_count;
+
+        SET loop_count = (loop_count + 1);
+    END WHILE;
+
 END;
 $$
