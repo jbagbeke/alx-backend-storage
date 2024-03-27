2d9a6819b339ed8f4f8779bdb8a47a58393b8b1c
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-27 02:05:11 +0000

    0x01. NoSQL Mandatory Task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -4,15 +4,12 @@ MongoDB - Lists all documents in a collection
 """
 import pymongo
 
-if __name__ == '__main__':
-    def list_all(mongo_collection):
-        """
-        Lists all docs in a mongodb collection
-        """
-        
-        if not mongo_collection:
-            return []
-
-        doc_list = mongo_collection.find()
-        return [doc for doc in doc_list]    
+def list_all(mongo_collection):
+    """
+    Lists all docs in a mongodb collection
+    """
     
+    if not mongo_collection:
+        return []
+
+    return [doc for doc in mongo_collection.find()]
