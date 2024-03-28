0c46e7d5105910c59c8a33783ca8086af1026019
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-28 19:21:21 +0000

    0x02. Redis basic Mandatory Task 1

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 Creates a redis client instance class
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Any
 from uuid import uuid4
 
 
@@ -27,3 +27,32 @@ class Cache:
         self._redis.set(redis_key, data)
 
         return redis_key
+
+    @property
+    def get(self, key: str, fn: Callable=None) -> Any:
+        """
+        Converts data to back to desired format
+        """
+
+        redis_response = self._redis.get(key)
+
+        if redis_response:
+            if fn and fn == int:
+                return self.get_int(redis_response)
+            elif fn and fn == str:
+                return self.get_str(redis_response)
+
+            return redis_response
+
+    def get_str(self, redis_response: str) -> str:
+        """
+        Decodes redis response with utf-8
+        """
+        return redis_response.decode('utf-8')
+
+    def get_int(self, redis_response: str) -> int:
+        """
+        Decodes redis response with utf-8 and type casts to int
+        """
+
+        return int(redis_response.decode('utf-8'))
