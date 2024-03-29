aa54e1a0fadf510c9cd58596660e1cc53509fd5e
Joshua Bubune Agbeke <joshua.legithive@gmail.com>
2024-03-29 21:52:48 +0000

    0x02. Redis basic Mandatory Task 3

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -16,15 +16,33 @@ def count_calls(method: Callable) -> Callable:
     @wraps(method)
     def cache_method_wrapper(self, *args, **kwargs):
         method_calls = self._redis.get(str(method.__qualname__))
-        
+
         if method_calls:
             self._redis.incr(str(method.__qualname__))
         else:
             self._redis.set(str(method.__qualname__), 1)
-        
+
         redis_key = method(self, *args, **kwargs)
         return redis_key
     return cache_method_wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """
+    Stores method params and outputs when the method is called
+    """
+    
+    @wraps(method)
+    def method_call_history(self, *args, **kwargs):
+        inputs_key = str(method.__qualname__) + ":inputs"
+        outputs_key = str(method.__qualname__) + ":outputs"
+        
+        self._redis.rpush(inputs_key, str(args))
+        redis_key_output = method(self, *args, **kwargs)
+        self._redis.rpush(outputs_key, redis_key_output)
+        
+        return redis_key_output
+    return method_call_history
 
 
 class Cache:
@@ -39,6 +57,7 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
